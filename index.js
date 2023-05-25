24fad0ba84990f1b276cf4dd2e0c23a6c5da217a
wambui-nyambura <nyamburawambuui@gmail.com>
2023-05-25 20:25:58 +0300

    added a fruit and vegetable to the list

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ document.getElementById('body').style.backgroundColor = 'silver'
 document.getElementById('title').style.color = 'green'
 document.getElementById('title2').style.textTransform = 'uppercase'
 document.getElementById('title3').style.textTransform = 'uppercase'
+
+let fruit = document.getElementById('fruList');
+let fruits = document.createElement('li');
+fruits.textContent = 'Pepino';
+fruit.appendChild(fruits);
+let veggetableList = document.getElementById('vegList');
+let newVeggetables = document.createElement('li');
+newVeggetables.textContent = 'Cucumber';
+veggetableList.appendChild(newVeggetables)
