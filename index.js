c7348ac45ae3966c31ae328b920668cbe622efc5
wambui-nyambura <nyamburawambuui@gmail.com>
2023-05-25 20:15:18 +0300

    Dom asg

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('body');
+document.getElementById('body').style.backgroundColor = 'silver'
+document.getElementById('title').style.color = 'green'
+document.getElementById('title2').style.textTransform = 'uppercase'
+document.getElementById('title3').style.textTransform = 'uppercase'
